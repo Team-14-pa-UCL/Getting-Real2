91e0f07a54938803fd51a965b1fba24d0a0c6979
Martin Lindecrona <martinhersomhansen@gmail.com>
2025-05-07 21:50:37 +0200

    Updatering med Kennies.

---
+++ Application/Umove AS/Models/Block.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umove_AS.Models
+{
+    internal class Block
+    {
+        DateTime blockStart;
+        DateTime blockEnd;
+        int blockID;
+        int blockLength;
+        int blockPause;
+        int routeLenghtInKm;
+        int blockTotalDistanceInKm;
+        int blockTotalTimeInService;
+        int blockTotalTime;
+    }
+}
