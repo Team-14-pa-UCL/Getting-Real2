16e5bb68c53e1280e8db6bba4b26568116bcfbc4
Wuway <contactdieter@gmail.com>
2025-05-10 18:24:54 +0200

    tilføjet DataHandler klasse i Services Opdateret ShiftPlan klassen med FromString metode til brug af LoadShiftplans metoden in DataHanlderen

--- Application/Umove AS/Models/Bus.cs
+++ Application/Umove AS/Models/Bus.cs
@@ -15,13 +15,13 @@ namespace Umove_AS.Models
         public BusStatus Status { get; private set; } //Ny 04-05-2025 (Bruger enum)
         public double BatteryCapacity { get; private set; }
         public double KmPerKWh { get; private set; } //DK(nyt property navn)
-        public double KmPerLiter { get; private set; } //DK
-        public double CurrentChargeKWh { get; private set; } //DK
-        public int CurrentChargePercent {get; private set; } //DK
-        public double ChargerSpeed { get; private set; } //DK
+        public double KmPerLiter { get; private set; } 
+        public double CurrentChargeKWh { get; private set; } 
+        public int CurrentChargePercent {get; private set; } 
+        public double ChargerSpeed { get; private set; } 
         public TimeSpan OperationalTime { get; private set; }
-        public string Location { get; private set; } //DK
-        public double AverageSpeed { get; private set; }//DK
+        public string Location { get; private set; } 
+        public double AverageSpeed { get; private set; }
         public ShiftPlan ShiftPlan { get; private set; }
 
         
